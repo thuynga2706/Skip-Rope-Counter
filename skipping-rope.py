1ce14001be0b948acffef0abc2e1be6fcd3df7e7
Nga Nguyen <thuynga2706@gmail.com>
2022-01-02 22:49:14 +0100

    feat: tracking progress added

--- skipping-rope.py
+++ skipping-rope.py
@@ -2,7 +2,7 @@
 # If we ever meet in the future, I would like to invite you for a coffee to discuss further possibilites of AI and automation
 # Now enjoy finding bugs and helping me improve my code!
 
-import time
+import time,csv
 from datetime import datetime
 
 import cv2
@@ -26,7 +26,7 @@ COUNTER = 0
 STAGE = "down"
 TIME_ELAPSED = 0
 PREV_COORDINATE=0
-
+DIFF = 0.02
 #Circles & Texts
 CIRCLE_COLOR = (189, 158, 255)#I love pink, especially pastel pink
 CENTER1 = (50, 50) #Counter
@@ -63,19 +63,18 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         try:
             landmarks = results.pose_landmarks.landmark         
             # Get coordinates of left shoulder, it can be any point on body
-            #shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_PINKY.value].x,landmarks[mp_pose.PoseLandmark.LEFT_PINKY.value].y]
-            visibility = landmarks[mp_pose.PoseLandmark.LEFT_PINKY.value].visibility
+            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            visibility = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].visibility
             if visibility>0.6:
                 # Visualize
                 cv2.putText(image, str(round(shoulder[1],3)), 
                                 tuple(np.multiply(shoulder, [640, 480]).astype(int)), 
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
                 # Skipping rope Counter Logic   
-                if shoulder[1]< PREV_COORDINATE-0.04 and STAGE == "down":
+                if shoulder[1]< PREV_COORDINATE-DIFF and STAGE == "down":
                     STAGE = "up"
                     #print("up")          
-                if shoulder[1]> PREV_COORDINATE+0.04 and STAGE == "up":
+                if shoulder[1]> PREV_COORDINATE+DIFF and STAGE == "up":
                     STAGE = "down"
                     COUNTER +=1
                     #print("down")
@@ -94,7 +93,7 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         circle(STAGE,CENTER2,HEADER2)
         circle(sec_to_min(seconds=TIME_ELAPSED),CENTER3,HEADER3)
 
-        # Render detections
+        # Detections
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
                                 mp_drawing.DrawingSpec(color=(255, 134, 133), thickness=2, circle_radius=2), 
                                 mp_drawing.DrawingSpec(color=(189, 158, 255), thickness=2, circle_radius=2) 
@@ -102,6 +101,13 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         cv2.imshow('Skipping Rope', image)
         cv2.imshow('Camera', frame)
         if cv2.waitKey(10) & 0xFF == ord('q'):
+            try:
+                info = [datetime.now().strftime("%Y/%m/%d %H:%M:%S"),COUNTER,round(TIME_ELAPSED),round(COUNTER/(TIME_ELAPSED/60))]
+                with open('tracking-progress.csv', mode='a',newline='') as f:
+                    writer = csv.writer(f)
+                    writer.writerow(info)
+            except:
+                pass
             break
 
     cap.release()
